9fc9f4244ba513c22929a1511c26322985c4d004
fugaswolf <alvi.istamalov@gmail.com>
2023-04-11 14:27:21 +0200

    initial project add. Closes #1

---
+++ src/main/java/guru/springframework/mssccoffeeservice/MsscCoffeeServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.mssccoffeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscCoffeeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscCoffeeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/mssccoffeeservice/MsscCoffeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mssccoffeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscCoffeeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
